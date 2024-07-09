e0eb26939c9de26b99e4dc2b0d9f7b9171f6342e
Alvaro Zacarias <alvaro.zacmat@gmail.com>
2024-07-08 19:38:28 -0600

    Design done. Active States and accessibility missing

---
+++ index.js
@@ -0,0 +1,12 @@
+
+const mobileMenu = document.getElementById('mobile-menu');
+
+// Open and Close mobile menu
+
+function openMenu() {
+        mobileMenu.style.display = 'block';
+}
+
+function closeMenu() {
+    mobileMenu.style.display = 'none';
+}
